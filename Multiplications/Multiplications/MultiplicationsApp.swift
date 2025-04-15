c239771adc00003d08aea5baf8a7f53667215aef
Lorenzo Ilardi <lo.ilardi.dev@gmail.com>
2025-04-15 19:56:59 +0200

    Initial project setup

---
+++ Multiplications/Multiplications/MultiplicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationsApp.swift
+//  Multiplications
+//
+//  Created by Lorenzo Ilardi on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
