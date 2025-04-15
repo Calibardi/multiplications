cdc51d72ecc645a999947226b071792524a86515
Lorenzo Ilardi <lo.ilardi.dev@gmail.com>
2025-04-15 19:57:57 +0200

    Rename main view

--- Multiplications/Multiplications/HomeView.swift
+++ Multiplications/Multiplications/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Multiplications
 //
 //  Created by Lorenzo Ilardi on 15/04/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
--- Multiplications/Multiplications/MultiplicationsApp.swift
+++ Multiplications/Multiplications/MultiplicationsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MultiplicationsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
